ae778666270768679ecfaf16226b289ce55d0a55
hamishHay <hhay@lpl.arizona.edu>
2015-06-19 13:14:40 -0300

    Added slight framework for input parameters and nodes

---
+++ inputs.py
@@ -0,0 +1,10 @@
+#! python
+
+class Input:
+
+    def __init__(self):
+        #default (Titan) values
+        self.a = 1221865e3
+
+    def ReadInputs(self, FileName):
+        self.a = 0
---
+++ node.py
@@ -0,0 +1,27 @@
+#! python
+
+import subprocess as subp
+
+class Node:
+    
+    def __init__(self, depth, coeff, data, cpy_init):
+        self.h = depth
+        self.alpha = coeff
+        self.inputs = data
+        self.copy_init = cpy_init
+        self.running = False
+
+    def MakeInputFile():
+        print('Hi')
+
+    def CreateDirectory():
+        print('Hi')
+
+    def CopyInitialConditions():
+        print('Hi')
+
+    def IsRunning():
+        print('Hi')
+
+    
+
