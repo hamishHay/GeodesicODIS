797a778dd4d507231b261b2e16b2ffdc12c1d214
hamishHay <hhay@lpl.arizona.edu>
2015-08-27 11:05:44 -0700

    Some tidying

--- GridSearch/Process.py
+++ GridSearch/Process.py
@@ -14,10 +14,10 @@ class Process:
         self.complete = False
         self.node = Node
         self.childrenPos = []
-        self.directory = os.getcwd() + "\\h" + str(self.h) + "_alpha" + str(self.a)
+        self.directory = os.getcwd() + "/h" + str(self.h) + "_alpha" + str(self.a)
         self.results_dir = self.directory #os.getcwd() + "\\Results\\h" + str(self.h) + "_alpha" + str(self.a)
         self.init = "true"
-        self.ODIS_PATH = /source/ODIS/ODIS
+        self.ODIS_PATH = "/source/ODIS/BUILD/ODIS"
 
         if self.h <= 35:
             if self.node[0] == 0 and self.node[1] == dim[1] - 1: #top and right
@@ -46,30 +46,9 @@ class Process:
                 self.childrenPos.append((self.node[0],self.node[1]+1))
                 self.max_children = 1
 
-        self.latnum = 90
+        self.latnum = 72
         self.lonnum = 45
-        self.dt = 100
-
-        if (self.h < 35):
-            self.latnum = 90 #2
-        elif (self.h < 100):
-            self.latnum = 60 # 3
-        elif (self.h < 1000):
-            self.latnum = 60 #3
-        elif (self.h < 10000):
-            self.latnum = 90 # 2
-        elif (self.h <= 50000):
-            self.latnum = 90 # 2
-        else:
-            self.latnum = 90 # 2
-            self.dt = 25
-
-        if (self.a >= 6e-8):
-            self.dt = 105
-        #elif (self.a >= 1e-8):
-        #    self.dt = 64
-        else:
-            self.dt = 40
+        self.dt = 40
 
 
     def __repr__(self):
@@ -87,12 +66,12 @@ class Process:
         self.CreateInputFile()
 
     def CreateInputFile(self):
-        f = open(self.directory+"\\input.in",'w')
+        f = open(self.directory+"/input.in",'w')
         f.write("ocean thickness; \t \t \t " + str(self.h) +"; \t \t \t h; \n")
         f.write("friction coefficient; \t \t \t " + str(self.a) +"; \t \t \t alpha; \n")
         f.write("potential; \t \t \t OBLIQ; \t \t \t potential; \n")
         f.write("simulation end time; \t \t \t " + str(100) +"; \t \t \t endTime; \n")
-
+        f.write("friction type; \t \t \t QUADRATIC; \t \t \t friction;" )
 
         f.write("time step; \t \t \t " + str(self.dt) +"; \t \t \t timeStep; \n")
         f.write("latitude spacing; \t \t \t " + str(self.latnum) +"; \t \t \t lat; \n")
