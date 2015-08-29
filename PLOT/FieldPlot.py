9914a0b70bb8f4a0c7001bf5f500743b5155b8a1
hamishHay <hhay@lpl.arizona.edu>
2015-08-29 16:06:29 -0700

    Updated Plotted Script
    Now plotting can be completed in a friendly object based manner

--- PLOT/FieldPlot.py
+++ PLOT/FieldPlot.py
@@ -8,7 +8,7 @@ import os
 import sys
 
 class ODISPlot:
-    def __init__(self,orbitnum=0,figdim=(1,2),figsize=(10,3),saveName="ODISPLOT.png"):
+    def __init__(self,orbitnum=0,figdim=(1,2),figsize=(10,3),saveName="ODISPLOT"):
         # Set matplotlib text options
         plt.rc('font', family='serif')
         plt.rc('font',serif='Palatino')
@@ -27,6 +27,7 @@ class ODISPlot:
         self.labelSize = 10
         self.slabelSize = 9
         self.titleSize = 11
+        self.supTitleSize = 12
 
         # Set plotting orbit
         self.orbit = orbitnum
@@ -45,7 +46,7 @@ class ODISPlot:
     def loadFieldAxisOptions(self,ax):
         plt.rcParams['contour.negative_linestyle'] = 'solid'
 
-        self.xticks=[0,90,180,270,360]
+        self.xticks=[0,45,90,135,180,225,270,315,360]
         self.yticks=[90,60,30,0,-30,-60,-90]
 
         ax.xaxis.set_ticks( self.xticks )
@@ -59,24 +60,27 @@ class ODISPlot:
         ax.axis([0, 360, -90, 90])
         ax.set_aspect('equal')
 
-    def loadFieldColourOptions(self,cb,ax,label):
+    def loadFieldColourOptions(self,cb,ax,cticks=[]):
         cb.ax.tick_params(axis='both', which='major', labelsize=self.labelSize)
         cb.outline.set_linewidth(0.7)
-        cb.set_label(label,fontsize=self.labelSize)
+        cb.set_label(self.caxisLabel,fontsize=self.labelSize)
+        if len(cticks)>0:
+            cb.set_ticks(cticks)
 
-    def ReadData(self,field="displacement"):
+    def ReadData(self,field="displacement",directory = os.getcwd()):
         orbitnum=str(self.orbit)
 
-        directory = os.getcwd()
         name = ["/EastVelocity/u_vel_"+orbitnum+".txt", "/NorthVelocity/v_vel_"+orbitnum+".txt", "/Displacement/eta_"+orbitnum+".txt"]
 
         if field=="displacement":
             name = name[2]
-            self.field = "Displacement"
+            self.caxisLabel = "Dispclacement, $\\eta$ (m)"
         elif field=="north_vel":
             name = name[1]
+            self.caxisLabel = "North Velocity, $v$ (\\si{\\metre\\per\\second})"
         elif field=="east_vel":
             name = name[0]
+            self.caxisLabel = "East Velocity, $v$ (\\si{\\metre\\per\\second})"
         else:
             print("Field " + field + "not recognised. Exiting.")
             sys.exit()
@@ -99,75 +103,79 @@ class ODISPlot:
 
         return self.data
 
-    def PlotField(self,cformat=2):
+    def PlotField(self,cformat=2,cticks=[]):
         self.currentFig += 1
 
         ax = self.fig.add_subplot(self.figDim[0],self.figDim[1],self.currentFig)
-        p1 = ax.pcolormesh(self.x,self.y,self.data,cmap=self.cmap,rasterized=True)
+        self.loadFieldAxisOptions(ax)
+
+        p1 = ax.pcolormesh(self.x,self.y,self.data,cmap=self.cmap,rasterized=False)
         divider = make_axes_locatable(ax)
         cax = divider.append_axes("bottom", size="5%", pad=0.5)
         c1 = plt.colorbar(p1, cax = cax, format='%.' +str(cformat) + 'f',orientation="horizontal")
+
         cs1 = ax.contour(self.x,self.y,self.data,8,colors='k',linewidths=0.4)
 
-        label = "Dispclacement, $\\eta$ (m)"
-        self.loadFieldColourOptions(c1,ax,label)
-        self.loadFieldAxisOptions(ax)
+        self.loadFieldColourOptions(c1,ax,cticks)
 
-    def PlotVelocity(self,u,v,cformat=3):
+
+    def PlotVelocity(self,u,v,cformat=3,scale=1/0.02,cticks=[]):
         self.currentFig += 1
+        self.caxisLabel = "Velocity, $\\left|\\mathbf{u}\\right|$ (\\si{\\metre\\per\\second})"
 
         u=u[:-1][:]
 
         mag = np.sqrt(u**2+v**2)
 
-        ax = self.fig.add_subplot(self.figDim[0],self.figDim[1],self.currentFig)
         x = np.linspace(0,360,len(u[0]))
-        y = np.linspace(90,-90,len(u))
+        y = np.linspace(-90,90,len(u))
 
-        p1 = ax.pcolormesh(x,y,mag,cmap=self.cmap,vmin=0,rasterized=True)
+        ax = self.fig.add_subplot(self.figDim[0],self.figDim[1],self.currentFig)
+        p1 = ax.pcolormesh(x,y,mag,cmap=self.cmap,vmin=0,rasterized=False)
         cs1 = ax.contour(x,y,mag,8,colors='k',linewidths=0.4)
 
         func_u = interp2d(x,y,u,kind='cubic')
         func_v = interp2d(x,y,v,kind='cubic')
 
         x = np.linspace(0,360,18)
-        y = np.linspace(90,-90,16)
+        y = np.linspace(-90,90,16)
 
         u = func_u(x,y)
         v = func_v(x,y)
 
-        ax.quiver(x,y,u,v,pivot='middle',width=0.002,headwidth=3,scale=1/0.2)
+        ax.quiver(x,y,u,v,pivot='middle',width=0.002,headwidth=3,scale=scale)
 
         divider = make_axes_locatable(ax)
         cax = divider.append_axes("bottom", size="5%", pad=0.5)
         c1 = plt.colorbar(p1, cax = cax, format='%.' +str(cformat) + 'f',orientation="horizontal")
-        c1.ax.tick_params(axis='both', which='major', labelsize=self.labelSize)
-        c1.set_ticks([0,0.01,0.02,0.03,0.04])
-        c1.outline.set_linewidth(0.7)
-        c1.set_label("Velocity, $\\mathbf{u}$ (\\si{\\metre\\per\\second})",fontsize=self.labelSize)
 
+        self.loadFieldColourOptions(c1,ax,cticks)
         self.loadFieldAxisOptions(ax)
-    
+
+    def SetSuperTitle(self, title):
+        self.fig.suptitle(title,fontsize=self.supTitleSize)
+
     def ShowFig(self):
         plt.show()
         plt.close()
 
-###### DEFINE FIG #######
+    def SaveFig(self):
+        self.fig.savefig(self.saveName +  '.pdf',bbox_inches='tight')
+        self.fig.savefig(self.saveName +  '.png', format='PNG',bbox_inches='tight',dpi=800)
 
-#fig = plt.figure(1,figsize=(10,3))
+if __name__=="__main__":
 
-ODIS = ODISPlot(orbitnum=20,figdim=(1,2))
+    ODIS = ODISPlot(orbitnum=20,figdim=(1,2),figsize=(10,3))
 
-ODIS.ReadData("displacement")
-ODIS.PlotField()
+    ODIS.ReadData("displacement")
+    ODIS.PlotField(cticks = [-4,-2,0,2,4,6,8])
 
-data_v = ODIS.ReadData("north_vel")
-data_u = ODIS.ReadData("east_vel")
+    data_v = ODIS.ReadData("north_vel")
+    data_u = ODIS.ReadData("east_vel")
 
-ODIS.PlotVelocity(data_u,data_v,cformat=2)
+    ODIS.PlotVelocity(data_u,data_v,cformat=2,scale=1/0.3)
 
-ODIS.ShowFig()
+    ODIS.SetSuperTitle("c) Combined Eccentricity and Obliquity Tides")
 
-#fig.suptitle("Eccentricity-Radial Tide",fontsize=12)
-#fig.savefig('ecc_rad_numerical.pdf', format='PDF',bbox_inches='tight')
-#fig.savefig('ecc_rad_numerical.png', format='PNG',bbox_inches='tight',dpi=900)
+    ODIS.ShowFig()
+    ODIS.SaveFig()
