1172c3490d3713311b28599c6bf390cc66dfac1a
hamishHay <hhay@lpl.arizona.edu>
2016-09-13 21:42:33 -0700

    tab test

--- main.cpp
+++ main.cpp
@@ -43,48 +43,48 @@ using namespace std;
 
 int main(void)
 {
-	// Read in global constants from input.in file with 0, and use defaults
-	// with 1 (currently set to Titan parameters). All constants are stored in the
-	// class "Globals".
+  // Read in global constants from input.in file with 0, and use defaults
+  // with 1 (currently set to Titan parameters). All constants are stored in the
+  // class "Globals".
 
-	Globals * constants = new Globals(0);
+  Globals * constants = new Globals(0);
 
-	// Create the numerical grid using the minimum node spacing from "constants"
-	// Globals instance.
+  // Create the numerical grid using the minimum node spacing from "constants"
+  // Globals instance.
 
-	Mesh * grid = new Mesh(constants);
+  Mesh * grid = new Mesh(constants);
 
-	// Allocate the solvable "Fields". These are all the quantities which ODIS aims
-	// to calculate a solution for (velocity and surface displacement), as well as
-	// the tidal forcing components. Each field is allocated to a particular node
-	// in the grid, and is either staggered one node East or one node South of the
-	// parent cell.
+  // Allocate the solvable "Fields". These are all the quantities which ODIS aims
+  // to calculate a solution for (velocity and surface displacement), as well as
+  // the tidal forcing components. Each field is allocated to a particular node
+  // in the grid, and is either staggered one node East or one node South of the
+  // parent cell.
 
-	Field * u = new Field(grid,0,1); // Eastward velocity component. Staggered East.
-	Field * v = new Field(grid,1,0); // Northward velocity component. Staggered south
-	Field * eta = new Field(grid,0,0); // Surface displacement. Cell centered.
-	Field * dUlat = new Field(grid,1,0); // Latitudinal tidal potential gradient. Staggered south.
-	Field * dUlon = new Field(grid,0,1); // Longitudinal tidal potential gradient. Staggered east.
+  Field * u = new Field(grid,0,1); // Eastward velocity component. Staggered East.
+  Field * v = new Field(grid,1,0); // Northward velocity component. Staggered south
+  Field * eta = new Field(grid,0,0); // Surface displacement. Cell centered.
+  Field * dUlat = new Field(grid,1,0); // Latitudinal tidal potential gradient. Staggered south.
+  Field * dUlon = new Field(grid,0,1); // Longitudinal tidal potential gradient. Staggered east.
 
-	// Allocate memory for (hopefully) conserved quantities, mass and energy.
-	// Classes Mass and Energy use Field as a parent class, and must be passed the
-	// quantities on which they are derived.
+  // Allocate memory for (hopefully) conserved quantities, mass and energy.
+  // Classes Mass and Energy use Field as a parent class, and must be passed the
+  // quantities on which they are derived.
 
-	Mass * mass = new Mass(grid, 0, 0, constants, eta);
-	Energy * energy = new Energy(grid, 0, 0, constants, u, v, mass);
+  Mass * mass = new Mass(grid, 0, 0, constants, eta);
+  Energy * energy = new Energy(grid, 0, 0, constants, u, v, mass);
 
-	// Allocate space for class tupe Solver. Solver creates the environment to
-	// begin the numerical calculations, with pointer access to all necessary
-	// quantiies.
+  // Allocate space for class tupe Solver. Solver creates the environment to
+  // begin the numerical calculations, with pointer access to all necessary
+  // quantiies.
 
-	Solver * solution = new Solver(0, 1, constants, grid, dUlon, dUlat,  u, v, eta, energy);
+  Solver * solution = new Solver(0, 1, constants, grid, dUlon, dUlat,  u, v, eta, energy);
 
-	// Begin the calculations by calling the Solve member function. Probably
-	// overkill.
-	solution->Solve();
+  // Begin the calculations by calling the Solve member function. Probably
+  // overkill.
+  solution->Solve();
 
   // Calculations are finished or ODIS has terminated with an error. Return from
-	// main.
+  // main.
 
   return 0;
 }
