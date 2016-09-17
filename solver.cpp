6e6c26c6c04ecb285d96679423d7809180d78f96
hamishHay <hhay@lpl.arizona.edu>
2016-09-17 07:11:34 -0700

    comment

--- solver.cpp
+++ solver.cpp
@@ -1259,7 +1259,7 @@ void Solver::DumpFields(int output_num) {
 
   hid_t dataset = H5Dcreate(file, "test1", H5T_NATIVE_FLOAT, data_space, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
 
-  ret = H5Dwrite(dataset, H5T_NATIVE_FLOAT, H5S_ALL, H5S_ALL, H5P_DEFAULT, eta1D);
+  // ret = H5Dwrite(dataset, H5T_NATIVE_FLOAT, H5S_ALL, H5S_ALL, H5P_DEFAULT, eta1D);
 
 
   // hid_t plist = H5Pcreate(H5P_DATASET_CREATE);
