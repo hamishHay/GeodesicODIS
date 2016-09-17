67c28c0ba4c56927488458049c71eb189981a51b
hamishHay <hhay@lpl.arizona.edu>
2016-09-17 08:23:33 -0700

    fix test?

--- solver.cpp
+++ solver.cpp
@@ -1285,6 +1285,8 @@ void Solver::DumpFields(int output_num) {
 
   start[0] = 0;
 
+  data_space - H5Dget_space(dataset);
+
   H5Sselect_hyperslab(data_space, H5S_SELECT_SET, start, NULL, count, NULL);
 
   ret = H5Sselect_hyperslab(data_space, H5S_SELECT_SET, start, NULL, count, NULL);
