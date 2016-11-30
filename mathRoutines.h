f383fb37f33d1c73828a74a530c5068f9135c7ef
hamishHay <hhay@lpl.arizona.edu>
2016-11-30 11:26:04 -0700

    Factorial function now relies on a look-up table up to n=32

--- mathRoutines.h
+++ mathRoutines.h
@@ -5,13 +5,72 @@
 #include "solver.h"
 #include <math.h>
 
-inline int factorial(int n) __attribute__((always_inline));
-inline int factorial(int n)
+inline double gamm1n(double xx);
+inline double gamm1n(double xx) {
+  double x,y,tmp,ser;
+  static double cof[6]={76.18009172947146,
+                        -86.50532032941677,
+                        24.01409824083091,
+                        -1.231739572450155,
+                        0.1208650973866179e-2,
+                        -0.5395239384953e-5};
+  int j;
+
+  y=x=xx;
+  tmp=x+5.5;
+  tmp -= (x+0.5)*log(tmp);
+  ser=1.000000000190015;
+  for (j=0;j<=5;j++) ser += cof[j]/++y;
+  return -tmp+log(2.5066282746310005*ser/x);
+}
+
+inline double factrl(int n)
 {
-    if(n > 1)
-        return n * factorial(n - 1);
-    else
-        return 1;
+  double gammln(float xx);
+
+  static int ntop=4;
+  static float a[33]={1.0,
+                      1.0,
+                      2.0,
+                      6.0,
+                      24.0,
+                      120.0,
+                      720.0,
+                      5040.0,
+                      40320.0,
+                      362880.0,
+                      3628800.0,
+                      39916800.0,
+                      479001600.0,
+                      6227020800.0,
+                      87178291200.0,
+                      1307674368000.0,
+                      20922789888000.0,
+                      355687428096000.0,
+                      6402373705728000.0,
+                      1.21645100408832e+17,
+                      2.43290200817664e+18,
+                      5.109094217170944e+19,
+                      1.1240007277776077e+21,
+                      2.585201673888498e+22,
+                      6.204484017332394e+23,
+                      1.5511210043330984e+25,
+                      4.032914611266057e+26,
+                      1.0888869450418352e+28,
+                      3.048883446117138e+29,
+                      8.841761993739701e+30,
+                      2.652528598121911e+32,
+                      8.222838654177924e+33,
+                      2.6313083693369355e+35};
+
+  int j;
+
+  if (n > 32) return exp(gamm1n(n+1.0));
+  // while (ntop<n) {
+  //   j=ntop++;
+  //   a[ntop]=a[j]*ntop;
+  // }
+  return a[n];
 }
 
 // Numerical recipes function for calculating the associated Legendre
@@ -24,14 +83,19 @@ inline double assLegendre(int l, int m, double x) {
 
   if (m==0) delta = 1.0;
 
-  normalise = sqrt((2.0-delta)*(2.0*l+1.0)*factorial(l-m)/factorial(l+m));
+
+  normalise = sqrt((2.0-delta)*(2.0*l+1.0)*factrl(l-m)/factrl(l+m));
+
+  // if (m%2 != 0) normalise = -normalise;
+
+  // std::cout<<"l="<<l<<" m="<<m<<" norm="<<factrl(l-m)/factrl(l+m)<<std::endl;
 
   pmm = 1.0;
   if (m > 0)
   {
     somx2 = sqrt((1.0 - x)*(1.0+x));
     fact = 1.0;
-    for (i=1; i<m; i++)
+    for (i=1; i<=m; i++)
     {
       pmm *= -fact*somx2;
       fact += 2.0;
