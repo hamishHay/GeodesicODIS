73060abc10358ae26638d257b09d487f08d24aa4
hamishHay <hamishhay.lpl@gmail.com>
2016-12-03 18:56:53 +0000

    switched to new array

--- solver.cpp
+++ solver.cpp
@@ -1130,7 +1130,7 @@ int Solver::Explicit() {
 
       for (int i = 0; i < etaLatLen; i++) {
         for (int j = 0; j < etaLonLen; j++) {
-          etaOldArray[i][j] = oceanLoadingArray[i][j];
+          etaNewArray[i][j] = oceanLoadingArray[i][j];
         }
       }
 
