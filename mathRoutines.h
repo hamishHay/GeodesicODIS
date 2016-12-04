68bc6bef43fb65794d1f9dc3a7da4549d42032dd
hamishHay <hamishhay.lpl@gmail.com>
2016-12-04 14:19:20 +0000

    print right stuff

--- mathRoutines.h
+++ mathRoutines.h
@@ -103,7 +103,7 @@ inline double assLegendre(int l, int m, double x) {
 
   // if (m%2 != 0) normalise = -normalise;
 
-  std::cout<<"l="<<l<<" m="<<m<<" norm="<<factrl(l-m)/factrl(l+m)<<std::endl;
+  std::cout<<"l="<<l<<" m="<<m<<" norm="<<normalise<<std::endl;
 
   pmm = 1.0;
   if (m > 0)
