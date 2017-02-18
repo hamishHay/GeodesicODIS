213df237c7b5607100dadc822843114971a886ff
Hamish Hay <hhay@lpl.arizona.edu>
2017-02-17 17:22:34 -0700

    Set phase factor to (-1)^m in extractSHCoeff. Very important for obliquity tide!

--- extractSHCoeff.f95
+++ extractSHCoeff.f95
@@ -1,14 +1,14 @@
-
 subroutine ExtractSHCoeff(DATA, N, L_MAX, HARM)
   implicit none
 
   interface
-    subroutine SHExpandDH(grid, n, cilm, lmax, norm, sampling, csphase, lmax_calc)
+    subroutine SHExpandDH(grid, n, cilm, lmax, norm, sampling, csphase, lmax_calc, exitstatus)
       real*8, intent(in) :: grid(:,:)
       real*8, intent(out) :: cilm(:,:,:)
       integer, intent(in) :: n
       integer, intent(out) :: lmax
       integer, intent(in), optional :: norm, sampling, csphase, lmax_calc
+      integer, intent(out), optional :: exitstatus
     end subroutine SHExpandDH
   end interface
 
@@ -18,22 +18,9 @@ subroutine ExtractSHCoeff(DATA, N, L_MAX, HARM)
   real*8, intent(out) :: HARM(2,L_MAX+1,L_MAX+1)
   integer :: L_MAX_RETURN
   integer :: l,m
+  integer :: returnVAL
 
-  call SHExpandDH (grid=DATA, n=N, cilm=HARM, lmax=L_MAX_RETURN, lmax_calc=L_MAX, sampling=2)
-  !
-  ! do l=1,l_max+1
-  !   do m=1,l_max+1
-  !     write(*,*) HARM(1,l,m)
-  !   end do
-  ! end do
-  ! !
-  ! write(*,*) 'SIN:'
-  !
-  ! do l=1,l_max+1
-  !   do m=1,l_max+1
-  !     write(*,*) HARM(2,l,m)
-  !   end do
-  ! end do
+  call SHExpandDH (grid=DATA, n=N, cilm=HARM, lmax=L_MAX_RETURN, lmax_calc=L_MAX, csphase=-1, sampling=2, exitstatus=returnVAL)
 
   return
 end subroutine ExtractSHCoeff
