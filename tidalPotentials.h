912a1f8d5a2de3ec0e3e05dde3dc065aab1ad268
Hamish Hay <hhay@lpl.arizona.edu>
2017-06-13 13:57:08 -0700

    tidal potential slightly optimized

--- tidalPotentials.h
+++ tidalPotentials.h
@@ -14,7 +14,7 @@
 
 
 // Degree 2 component of the eccentricity tide (see Tyler 2011, Matsuyama 2014)
-void deg2Ecc(Mesh * grid, Array2D<double> *, double simulationTime, double radius, double omega, double ecc);
+void deg2Ecc(Mesh * grid, Array2D<double> &, double simulationTime, double radius, double omega, double ecc);
 
 // // Degree 2 component of the eccentricity-radial tide (see Tyler 2011, Matsuyama 2014)
 // void deg2EccRad(Field * DUlat, Field * DUlon, double simulationTime, double radius, double omega, double ecc);
