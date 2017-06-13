39a7a026b2886015a87aa0118bd7c0cebf52b73e
Hamish Hay <hhay@lpl.arizona.edu>
2017-06-13 13:58:49 -0700

    Linear drag added.

---
+++ drag.h
@@ -0,0 +1,12 @@
+#ifndef DRAG_H_INCDLUDED
+#define DRAG_H_INCDLUDED
+
+#include "mesh.h"
+#include "globals.h"
+#include "array2d.h"
+#include <math.h>
+#include <iostream>
+
+void linearDrag(int &, double &, Array2D<double> &, Array2D<double> &);
+
+#endif
