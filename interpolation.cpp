9ef5072144ac498cc2c00a0f2303e1053efd0732
Hamish Hay <hhay@lpl.arizona.edu>
2017-09-13 16:36:15 -0700

    ADDED: Files for interpolating between geodesic and lat-lon grid.

---
+++ interpolation.cpp
@@ -0,0 +1,18 @@
+#include "mesh.h"
+#include "globals.h"
+#include "array1d.h"
+#include "array2d.h"
+#include "array3d.h"
+#include <math.h>
+#include <iostream>
+
+int interpolateGG2LL(Globals * globals,
+                     Mesh * grid,
+                     Array2D<double> & ll_data,
+                     Array1D<double> & gg_data,
+                     Array3D<double> & V_inv,
+                     Array2D<int> & cell_ID)
+
+{
+
+};
