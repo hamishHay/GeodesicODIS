e625a26c7f75947378454280632d9e0f56e32ca0
Hamish Hay <hhay@lpl.arizona.edu>
2017-09-21 11:15:12 -0700

    ADDED: small value check for SH coeffs.

--- sphericalHarmonics.cpp
+++ sphericalHarmonics.cpp
@@ -98,8 +98,10 @@ void getSHCoeffsLL(Array2D<double> & data,
         {
                 if (m<=l)
                 {
-                    sh_coeffs(l, m, 0) = sh_coeffs_fort[index];
-                    sh_coeffs(l, m, 1) = sh_coeffs_fort[index + 1];
+                    if (fabs(sh_coeffs_fort[index]) > 1e-16) sh_coeffs(l, m, 0) = sh_coeffs_fort[index];
+                    else sh_coeffs(l, m, 0) = 0.0;
+                    if (fabs(sh_coeffs_fort[index+1]) > 1e-16) sh_coeffs(l, m, 1) = sh_coeffs_fort[index+1];
+                    else sh_coeffs(l, m, 1) = 0.0;
                 }
 
                 index += 2;
