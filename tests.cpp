6cc7d9012fd08dc79ff2fab06644b733973fff5c
Hamish Hay <hhay@lpl.arizona.edu>
2017-11-08 16:41:32 -0700

    ADDED: function for testing the spatial operators acting in ODIS.

---
+++ tests.cpp
@@ -0,0 +1,12 @@
+#include "mesh.h"
+#include "globals.h"
+#include "array2d.h"
+#include "array1d.h"
+#include <math.h>
+#include <iostream>
+
+
+void runOperatorTests(Globals * globals, Mesh * mesh)
+{
+    
+}
---
+++ tests.h
@@ -0,0 +1,13 @@
+#ifndef TESTS_H_INCDLUDED
+#define TESTS_H_INCDLUDED
+
+#include "mesh.h"
+#include "globals.h"
+#include "array2d.h"
+#include "array1d.h"
+#include <math.h>
+#include <iostream>
+
+void runOperatorTests(Globals *, Mesh *);
+
+#endif
