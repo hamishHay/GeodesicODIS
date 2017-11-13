00e108a853aea5ba8ff533834c4205a9ca533d4d
hamishHay <hamishhay.lpl@gmail.com>
2017-11-13 10:38:04 -0700

    ADDED: functions for initating beta, alpha, u and div u and grad beta

--- tests.h
+++ tests.h
@@ -10,4 +10,13 @@
 
 void runOperatorTests(Globals *, Mesh *);
 
+
+void setBeta(Array1D<double> &, int, int, int, Array3D<double> &, Array3D<double> &);
+
+void setU(Array2D<double> &, int, int, int, Array3D<double> &, Array3D<double> &, Array2D<double> &, Array2D<double> &);
+
+void setDivU(Array1D<double> &, int, int, int, double, Array3D<double> &, Array3D<double> &, Array2D<double> &, Array2D<double> &);
+
+void setGradBeta(Array2D<double> &, int, int, int, double, Array3D<double> &, Array3D<double> &, Array2D<double> &);
+
 #endif
