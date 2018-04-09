915a4594bdbd1ca266d84d9bf4eeb1247b0f0ffa
Hamish Hay <hhay@lpl.arizona.edu>
2018-04-09 11:38:24 -0700

    MODIFIED: several attempts to speed up this function, none of which worked.

--- interpolation.cpp
+++ interpolation.cpp
@@ -6,6 +6,22 @@
 #include <math.h>
 #include <iostream>
 
+// extern "C"{
+// // FORTRAN adds _ after all the function names
+// // and all variables are called by reference
+// double dgemv_( const char * TRANS,
+//                const int * m,
+//                const int * n,
+//                const double * alpha,
+//                const double * V,
+//                const int * ldv,
+//                const double * x,
+//                const int * incx,
+//                const double * beta,
+//                const double * y,
+//                const int * incy);
+// }
+
 /**
 *   @purpose    Function interpolates data from the geodesic grid to the regular
 *               lat-lon grid. Interpolation is biquadratic and takes the form
@@ -49,6 +65,12 @@ int interpolateGG2LL(Globals * globals,
     double d_vec[6];            // static data vector
     double c_vec[6];            // static coefficient vector
     double V_inv_mat[6][6];     // static Vandermonde inverse matrix
+    double c_vec2[6];
+
+    double V_inv_mat2[6*6];
+
+    double val;
+    double v0, v1, v2, v3, v4, v5;
 
     Array2D<int> * friend_list;
     friend_list = &(mesh->node_friends);
@@ -74,56 +96,120 @@ int interpolateGG2LL(Globals * globals,
             */
 
             // find number of friends
-            f_num = 6;
-            if ((*friend_list)(gg_ID, 5) == -1)
-            {
-                f_num = 5;
+            // f_num = 6;
+            // if ((*friend_list)(gg_ID, 5) == -1)
+            // {
+            //     f_num = 5;
+            //
+            //     // If cell is pentagon, use middle node for interpolation
+            //     d_vec[5] = gg_data(gg_ID);
+            // }
+            //
+            // // loop through friend nodes,
+            // for (k = 0; k < f_num; k++)
+            // {
+            //     // get friend ID
+            //     f = (*friend_list)(gg_ID, k);
+            //
+            //     // assign data vector to values of surrounding friends
+            //     d_vec[k] = gg_data(f);
+            // }
+            //
+            //
+            // /*------------------------------------------------------------------
+            // * STEP 3: FILL VANDERMONDE INVERSE MATRIX
+            // * ------------------------------------------------------------------
+            // */
+            //
+            // for (k = 0; k < 6; k++)
+            // {
+            //     // Unrolling inner loop. Potential speed benefit?
+            //     V_inv_mat[k][0] = V_inv(gg_ID, k, 0);
+            //     V_inv_mat[k][1] = V_inv(gg_ID, k, 1);
+            //     V_inv_mat[k][2] = V_inv(gg_ID, k, 2);
+            //     V_inv_mat[k][3] = V_inv(gg_ID, k, 3);
+            //     V_inv_mat[k][4] = V_inv(gg_ID, k, 4);
+            //     V_inv_mat[k][5] = V_inv(gg_ID, k, 5);
+            // }
+            //
+            // /*------------------------------------------------------------------
+            // * STEP 4: PERFORM MATRIX VECTOR DOT PRODUCT
+            // * ------------------------------------------------------------------
+            // */
+            //
+            // // This naive implementation of matrix multiplication may be
+            // // sufficiently fast for the small 6x6 matricies used here
+            //
+            // for (k = 0; k < 6; k++)
+            // {
+            //     c_vec[k] = 0.0;
+            //     for (l = 0; l < 6; l++)
+            //     {
+            //         c_vec[k] += V_inv_mat[k][l];// * d_vec[l];
+            //     }
+            // }
 
-                // If cell is pentagon, use middle node for interpolation
-                d_vec[5] = gg_data(gg_ID);
-            }
+            // f_num = 6;
+            // if ((*friend_list)(gg_ID, 5) == -1)
+            // {
+            //     f_num = 5;
+            //
+            //     // If cell is pentagon, use middle node for interpolation
+            //     // d_vec[5] = gg_data(gg_ID);
+            // }
+
+            // f = (*friend_list)(gg_ID, 0);
+            // d_vec[0] = gg_data(f);
+            //
+            // f = (*friend_list)(gg_ID, 1);
+            // d_vec[1] = gg_data(f);
+            //
+            // f = (*friend_list)(gg_ID, 2);
+            // d_vec[2] = gg_data(f);
+            //
+            // f = (*friend_list)(gg_ID, 3);
+            // d_vec[3] = gg_data(f);
+            //
+            // f = (*friend_list)(gg_ID, 4);
+            // d_vec[4] = gg_data(f);
+            //
+            // f = (*friend_list)(gg_ID, 5);
+            // if (f == -1) d_vec[5] = gg_data(gg_ID);
+            // else d_vec[5] = gg_data(f);
+            //
+            // // loop through friend nodes,
+            // for (k = 0; k < 6; k++)
+            // {
+            //     c_vec[k] = V_inv(gg_ID, k, 0) * d_vec[0] + V_inv(gg_ID, k, 1) * d_vec[1]
+            //                +V_inv(gg_ID, k, 2) * d_vec[2] + V_inv(gg_ID, k, 3) * d_vec[3]
+            //                +V_inv(gg_ID, k, 4) * d_vec[4] + V_inv(gg_ID, k, 5) * d_vec[5];
+            // }
+
+            f = (*friend_list)(gg_ID, 0);
+            v0 = gg_data(f);
+
+            f = (*friend_list)(gg_ID, 1);
+            v1 = gg_data(f);
+
+            f = (*friend_list)(gg_ID, 2);
+            v2 = gg_data(f);
+
+            f = (*friend_list)(gg_ID, 3);
+            v3 = gg_data(f);
+
+            f = (*friend_list)(gg_ID, 4);
+            v4 = gg_data(f);
+
+            f = (*friend_list)(gg_ID, 5);
+            if (f == -1) v5 = gg_data(gg_ID);
+            else v5 = gg_data(f);
 
             // loop through friend nodes,
-            for (k = 0; k < f_num; k++)
-            {
-                // get friend ID
-                f = (*friend_list)(gg_ID, k);
-
-                // assign data vector to values of surrounding friends
-                d_vec[k] = gg_data(f);
-            }
-
-
-            /*------------------------------------------------------------------
-            * STEP 3: FILL VANDERMONDE INVERSE MATRIX
-            * ------------------------------------------------------------------
-            */
-
             for (k = 0; k < 6; k++)
             {
-                // Unrolling inner loop. Potential speed benefit?
-                V_inv_mat[k][0] = V_inv(gg_ID, k, 0);
-                V_inv_mat[k][1] = V_inv(gg_ID, k, 1);
-                V_inv_mat[k][2] = V_inv(gg_ID, k, 2);
-                V_inv_mat[k][3] = V_inv(gg_ID, k, 3);
-                V_inv_mat[k][4] = V_inv(gg_ID, k, 4);
-                V_inv_mat[k][5] = V_inv(gg_ID, k, 5);
-            }
-
-            /*------------------------------------------------------------------
-            * STEP 4: PERFORM MATRIX VECTOR DOT PRODUCT
-            * ------------------------------------------------------------------
-            */
-
-            // This naive implementation of matrix multiplication may be
-            // sufficiently fast for the small 6x6 matricies used here
-            for (k = 0; k < 6; k++)
-            {
-                c_vec[k] = 0.0;
-                for (l = 0; l < 6; l++)
-                {
-                    c_vec[k] += V_inv_mat[k][l] * d_vec[l];
-                }
+                c_vec[k] = V_inv(gg_ID, k, 0) * v0 + V_inv(gg_ID, k, 1) * v1
+                           +V_inv(gg_ID, k, 2) * v2 + V_inv(gg_ID, k, 3) * v3
+                           +V_inv(gg_ID, k, 4) * v4 + V_inv(gg_ID, k, 5) * v5;
             }
 
             /*------------------------------------------------------------------
@@ -141,21 +227,6 @@ int interpolateGG2LL(Globals * globals,
                             + c_vec[3]*y
                             + c_vec[4]*x*y
                             + c_vec[5]*y*y;
-            //
-            // std::cout<<gg_ID<<' '<<x<<' '<<y<<std::endl;
-            // std::cout<<c_vec[0]<<' '<<c_vec[1]<<' '<<c_vec[2]<<' '<<c_vec[3]<<' '<<c_vec[4]<<' '<<c_vec[5]<<std::endl;
-            // std::cout<<V_inv_mat[0][0]<<' '<<V_inv_mat[0][1]<<' '<<V_inv_mat[0][2]<<' '<<V_inv_mat[0][3]<<' '<<V_inv_mat[0][4]<<' '<<V_inv_mat[0][5]<<std::endl;
-            // std::cout<<V_inv_mat[1][0]<<' '<<V_inv_mat[1][1]<<' '<<V_inv_mat[1][2]<<' '<<V_inv_mat[1][3]<<' '<<V_inv_mat[1][4]<<' '<<V_inv_mat[1][5]<<std::endl;
-            // std::cout<<V_inv_mat[2][0]<<' '<<V_inv_mat[2][1]<<' '<<V_inv_mat[2][2]<<' '<<V_inv_mat[2][3]<<' '<<V_inv_mat[2][4]<<' '<<V_inv_mat[2][5]<<std::endl;
-            // std::cout<<V_inv_mat[3][0]<<' '<<V_inv_mat[3][1]<<' '<<V_inv_mat[3][2]<<' '<<V_inv_mat[3][3]<<' '<<V_inv_mat[3][4]<<' '<<V_inv_mat[3][5]<<std::endl;
-            // std::cout<<V_inv_mat[4][0]<<' '<<V_inv_mat[4][1]<<' '<<V_inv_mat[4][2]<<' '<<V_inv_mat[4][3]<<' '<<V_inv_mat[4][4]<<' '<<V_inv_mat[4][5]<<std::endl;
-            // std::cout<<V_inv_mat[5][0]<<' '<<V_inv_mat[5][1]<<' '<<V_inv_mat[5][2]<<' '<<V_inv_mat[5][3]<<' '<<V_inv_mat[5][4]<<' '<<V_inv_mat[5][5]<<std::endl;
-            // //
-            // char c;
-            //
-            // std::cin>>c;
-
-            // dgemv_(&TRANS, &M, &N, &ALPHA, V_inv_mat, &LDA, d_vec, &INCX, &BETA, c_vec, &INCY);
 
         }
     }
