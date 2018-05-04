bc27bac93e95e21ccaa6d55697bc6b7560e245ea
Hamish Hay <hhay@lpl.arizona.edu>
2018-05-04 16:25:36 -0700

    ADDED: options to compute gradient of tidal potential numerically.

--- tidalPotentials.cpp
+++ tidalPotentials.cpp
@@ -24,7 +24,7 @@
 void deg2Ecc(Mesh * grid, Array2D<double> & velocity, double simulationTime, double radius, double omega, double ecc)
 {
     double cosM, sinM, factor;              // cos(Mean anomaly), sin(Mean anomaly)
-    double * sin2Lat, * sin2Lon, *cos2Lon, *cosLat;
+    double *sinLat, * sin2Lat, * sin2Lon, *cos2Lon, *cosLat;
     int i,j, node_num;
     double * val;
     double * m;
@@ -47,23 +47,40 @@ void deg2Ecc(Mesh * grid, Array2D<double> & velocity, double simulationTime, dou
 
     // Assign pointers to start of trig node arrays
     cosLat = &(grid->trigLat(0,0));
+    sinLat = &(grid->trigLat(0,1));
     cos2Lon = &(grid->trig2Lon(0,0));
     sin2Lon = &(grid->trig2Lon(0,1));
     sin2Lat = &(grid->trig2Lat(0,1));
 
     // Solve for dUdlon
+    // for (i=0; i<node_num; i++) {
+    //     j = i*2;
+    //
+    //     velocity(i,0) = factor * 1.5 * cosLat[j]   // cosLat here as cos^2(lat)/cos(lat)
+    //                     * (4.*sinM * cos2Lon[j]
+    //                     - 3.*cosM * sin2Lon[j]);
+    //
+    //     velocity(i,1) = -factor*0.75*sin2Lat[j]
+    //                     *(3.*cosM*(1.+cos2Lon[j])
+    //                     + 4.*sinM*sin2Lon[j]);
+    //
+    // }
+
+    Array1D<double> * potential;
+    potential = new Array1D<double>(node_num);
     for (i=0; i<node_num; i++) {
         j = i*2;
 
-        velocity(i,0) = factor * 1.5 * cosLat[j]   // cosLat here as cos^2(lat)/cos(lat)
-                        * (4.*sinM * cos2Lon[j]
-                        - 3.*cosM * sin2Lon[j]);
+        (*potential)(i) = factor * radius * (-3./4. * (3.*sinLat[j]*sinLat[j] - 1.)* cosM    // cosLat here as cos^2(lat)/cos(lat)
+                        + 3./4. * cosLat[j] * cosLat[j] * (4. * sinM * sin2Lon[j] + 3. * cosM * cos2Lon[j]));
 
-        velocity(i,1) = -factor*0.75*sin2Lat[j]
-                        *(3.*cosM*(1.+cos2Lon[j])
-                        + 4.*sinM*sin2Lon[j]);
-
+        velocity(i, 0) = 0.0;
+        velocity(i, 1) = 0.0;
     }
+
+    pressureGradient(grid, velocity, *potential, node_num, -1.0);
+
+    delete potential;
 
 };
 
@@ -313,7 +330,7 @@ void deg2EccRad(Mesh * grid, Array2D<double> & velocity, double simulationTime, 
 */
 void deg2Obliq(Mesh * grid, Array2D<double> & velocity, double simulationTime, double radius, double omega, double theta)
 {
-    double * sinLat, * sinLon, *cosLon, *cosLat, * cos2Lat;
+    double * sinLat, * sin2Lat, * sinLon, *cosLon, *cosLat, * cos2Lat;
     int i,j, node_num;
     double * val;
     double * m;
@@ -321,11 +338,8 @@ void deg2Obliq(Mesh * grid, Array2D<double> & velocity, double simulationTime, d
 
     node_num = grid->node_num;
 
-    // factor = pow(omega,2.0)*pow(radius,2.0)*ecc;
     factor = -3. * grid->globals->loveReduct.Value() * pow(omega,2.0)*radius*theta;
     cosM = cos(omega*simulationTime);
-    // sinM = sin(omega*simulationTime);
-
 
     if (grid->globals->surface_type == LID_LOVE || grid->globals->surface_type == LID_MEMBR)
     {
@@ -336,21 +350,37 @@ void deg2Obliq(Mesh * grid, Array2D<double> & velocity, double simulationTime, d
     // Assign pointers to start of trig node arrays
     cosLat = &(grid->trigLat(0,0));
     cos2Lat = &(grid->trig2Lat(0,0));
+    sin2Lat = &(grid->trig2Lat(0,1));
     cosLon = &(grid->trigLon(0,0));
     sinLon = &(grid->trigLon(0,1));
     sinLat = &(grid->trigLat(0,1));
 
     // Solve for dUdlon
+    // for (i=0; i<node_num; i++) {
+    //     j = i*2;
+    //     // calculate potential gradient in longitude
+    //     velocity(i,0) = -factor * sinLat[j] * sinLon[j] * cosM;
+    //
+    //
+    //     // calculate potential gradient in latitude
+    //     velocity(i,1) = factor * cos2Lat[j] * cosLon[j] * cosM;
+    //
+    // }
+
+    Array1D<double> * potential;
+    potential = new Array1D<double>(node_num);
     for (i=0; i<node_num; i++) {
         j = i*2;
-        // calculate potential gradient in longitude
-        velocity(i,0) = -factor * sinLat[j] * sinLon[j] * cosM;
 
+        (*potential)(i) = factor/2.0 * radius * sin2Lat[j] * cosLon[j] * cosM;
 
-        // calculate potential gradient in latitude
-        velocity(i,1) = factor * cos2Lat[j] * cosLon[j] * cosM;
-
+        velocity(i, 0) = 0.0;
+        velocity(i, 1) = 0.0;
     }
+
+    pressureGradient(grid, velocity, *potential, node_num, -1.0);
+
+    delete potential;
 };
 
 /* Degree 2 component of the obliquity tide (see Tyler 2011, Matsuyama 2014)
@@ -616,324 +646,3 @@ void deg2EccTotal(Mesh * grid, Array2D<double> & velocity, double simulationTime
 
     // avgAtPoles(grid, velocity);
 };
-
-// OLD VERSION ----------------------------------------------------------------
-
-
-// /* Degree 2 component of the eccentricity-radial tide (see docs)
-//  *
-//  * Finds the gradient components of the degree-3 ecc-radial tide for Fields
-//  * DUlat and DUlon. Only current simulation required is absolute simulation
-//  * time. The function directly writes the solution arrays in both Field
-//  * classes. The radial time contains only the P_20 term of eccentricity tide. As
-//  * the order of potential expansion is zero (m=0), this potential only depends
-//  * on latitude.
-//  *
-//  *      inputs: Field * DUlat           Field for latitude gradient of potential
-//  *              Field * DUlon           Field for longitude gradient of potential
-//  *              double simulationTime   Current time in the simulation
-//  *              double radius           Satellite radius
-//  *              double omega            Satellite rotational angular speed
-//  *              double ecc              Satellite eccentricity (must be << 1)
-//  *
-// */
-// void deg2EccRad(Field * DUlat, Field * DUlon, double simulationTime, double radius, double omega, double ecc) {
-//     double cosM, factor;                    // cos(Mean anomaly),
-//     double * sin2Lat;                       // Pointer to 1D arrays for trig functions of latitude
-//     double ** latGrad, ** lonGrad;          // Pointer to 2D array solutions
-//     int i,j;
-//     int latLen, lonLen;
-//     double val;
-//
-//     factor = pow(omega,2.0)*pow(radius,2.0)*ecc;
-//     cosM = cos(omega*simulationTime);
-//
-//     // Get pointer to arrays
-//     latGrad = DUlat->solution;
-//     lonGrad = DUlon->solution;
-//
-//     // Set variables for dUlat solution
-//     latLen = DUlat->fieldLatLen;
-//     lonLen = DUlat->fieldLonLen;
-//
-//     sin2Lat = DUlat->sin2Lat;
-//
-//     // Solve for dUdlat
-//     for (i=0; i<latLen; i++) {
-//         val = -factor * (2.25 * sin2Lat[i] * cosM);
-//         for (j=0; j<lonLen; j++) {
-//             latGrad[i][j] = val;
-//         }
-//     }
-//
-//     // No need to solve for dUdLon as it is always zero in this case
-//
-// };
-//
-// /* Degree 2 component of the eccentricity-libration tide (Tyler, 2011)
-//  *
-//  * Finds the gradient components of the degree-3 ecc-lib tide for Fields
-//  * DUlat and DUlon. Only current simulation required is absolute simulation
-//  * time. The function directly writes the solution arrays in both Field
-//  * classes. The eccentricity-libration tide contains only the P_22 term from
-//  * the eccentricity tide.
-//  *
-//  *      inputs: Field * DUlat           Field for latitude gradient of potential
-//  *              Field * DUlon           Field for longitude gradient of potential
-//  *              double simulationTime   Current time in the simulation
-//  *              double radius           Satellite radius
-//  *              double omega            Satellite rotational angular speed
-//  *              double ecc              Satellite eccentricity (must be << 1)
-//  *
-// */
-// void deg2EccLib(Field * DUlat, Field * DUlon, double simulationTime, double radius, double omega, double ecc) {
-//     double cosM, sinM, factor;              // cos(Mean anomaly), sin(Mean anomaly)
-//     double * sin2Lat, * cosSqLat;           // Pointer to 1D arrays for trig functions of latitude
-//     double * cos2Lon, * sin2Lon;            // Pointer to 1D arrays for trig functions of longitude
-//     double ** latGrad, ** lonGrad;          // Pointer to 2D array solutions
-//     int i,j;
-//     int latLen, lonLen;
-//
-//     factor = pow(omega,2.0)*pow(radius,2.0)*ecc;
-//     cosM = cos(omega*simulationTime);
-//     sinM = sin(omega*simulationTime);
-//
-//     // Get pointer to arrays
-//     latGrad = DUlat->solution;
-//     lonGrad = DUlon->solution;
-//
-//     // Set variables for dUlat solution
-//     latLen = DUlat->fieldLatLen;
-//     lonLen = DUlat->fieldLonLen;
-//
-//     sin2Lat = DUlat->sin2Lat;
-//     cos2Lon = DUlat->cos2Lon;
-//     sin2Lon = DUlat->sin2Lon;
-//
-//     // Solve for dUdlat
-//     for (i=0; i<latLen; i++) {
-//         for (j=0; j<lonLen; j++) {
-//             latGrad[i][j] = -factor*0.75*sin2Lat[i]
-//                             *(3.*cosM*cos2Lon[j]
-//                             + 4*sinM*sin2Lon[j]);
-//             }
-//     }
-//
-//     // Set variable for dUlon solution
-//     latLen = DUlon->fieldLatLen;
-//     lonLen = DUlon->fieldLonLen;
-//
-//     cosSqLat = DUlon->cosSqLat;
-//     sin2Lon = DUlon->sin2Lon;
-//     cos2Lon = DUlon->cos2Lon;
-//
-//     // Solve for dUdlon
-//     for (i=0; i<latLen; i++) {
-//         for (j=0; j<lonLen; j++) {
-//             lonGrad[i][j] = factor* 1.5 * cosSqLat[i]
-//                             * (4.*sinM * cos2Lon[j]
-//                             - 3.*cosM * sin2Lon[j]);
-//         }
-//     }
-// };
-//
-// /* Degree 2 component of the obliquity tide (see Tyler 2011, Matsuyama 2014)
-//  *
-//  * Finds the gradient components of the degree-2 obliquity tide for Fields
-//  * DUlat and DUlon. Only current simulation required is absolute simulation
-//  * time. The function directly writes the solution arrays in both Field
-//  * classes.
-//  *
-//  *      inputs: Field * DUlat           Field for latitude gradient of potential
-//  *              Field * DUlon           Field for longitude gradient of potential
-//  *              double simulationTime   Current time in the simulation
-//  *              double radius           Satellite radius
-//  *              double omega            Satellite rotational angular speed
-//  *              double theta            Satellite obliquity in radians
-//  *
-// */
-// void deg2Obliq(Field * DUlat, Field * DUlon, double simulationTime, double radius, double omega, double theta)
-// {
-//     double cosM, factor;                    // cos(Mean anomaly)
-//     double * cosLat, * cos2Lat, * sinLat;   // Pointer to 1D arrays for trig functions of latitude
-//     double * cosLon, * sinLon;              // Pointer to 1D arrays for trug functions of longitude
-//     double ** latGrad, ** lonGrad;          // Pointer to 2D array solutions
-//     int i,j;
-//     int latLen, lonLen;
-//
-//     factor = -3.*pow(omega,2.0)*pow(radius,2.0)*theta;
-//     cosM = cos(omega*simulationTime);
-//
-//     // Get pointer to arrays
-//     latGrad = DUlat->solution;
-//     lonGrad = DUlon->solution;
-//
-//     // Set variables for dUlat solution
-//     latLen = DUlat->fieldLatLen;
-//     lonLen = DUlat->fieldLonLen;
-//
-//     cos2Lat = DUlat->cos2Lat;
-//     cosLon = DUlat->cosLon;
-//
-//     // Solve for dUdlat
-//     for (i=0; i<latLen; i++) {
-//         for (j=0; j<lonLen; j++) {
-//             latGrad[i][j] = factor*cos2Lat[i]*cosLon[j]*cosM;
-//         }
-//     }
-//
-//     // Set variable for dUlon solution
-//     latLen = DUlon->fieldLatLen;
-//     lonLen = DUlon->fieldLonLen;
-//
-//     cosLat = DUlon->cosLat;
-//     sinLat = DUlon->sinLat;
-//     sinLon = DUlon->sinLon;
-//
-//     // Solve for dUdlon
-//     for (i=0; i<latLen; i++) {
-//         for (j=0; j<lonLen; j++) {
-//             lonGrad[i][j] = -factor*cosLat[i]*sinLat[i]*sinLon[j]*cosM;
-//         }
-//     }
-//
-// };
-//
-// /* Degree 3 component of the obliquity tide (see docs)
-//  *
-//  * Finds the gradient components of the degree-3 obliquity tide for Fields
-//  * DUlat and DUlon. Only current simulation required is absolute simulation
-//  * time. The function directly writes the solution arrays in both Field
-//  * classes.
-//  *
-//  *      inputs: Field * DUlat           Field for latitude gradient of potential
-//  *              Field * DUlon           Field for longitude gradient of potential
-//  *              double simulationTime   Current time in the simulation
-//  *              double radius           Satellite radius
-//  *              double smAxis           Satellite semimajor axis
-//  *              double omega            Satellite rotational angular speed
-//  *              double theta            Satellite obliquity in radians
-//  *
-// */
-// void deg3Obliq(Field * DUlat, Field * DUlon, double simulationTime, double radius, double smAxis, double omega, double theta) {
-//     double cosM, factor;                                // cos(Mean anomaly)
-//     double * cosLat, * cosSqLat, * sinLat, * sinSqLat;  // Pointer to 1D arrays for trig functions of latitude
-//     double * cosSqLon, * sin2Lon;                       // Pointer to 1D arrays for trig functions of longitude
-//     double ** latGrad, ** lonGrad;                      // Pointer to 2D array solutions
-//     int i,j;
-//     int latLen, lonLen;
-//
-//     factor = pow(omega,2.0)*pow(radius,3.0)*theta/smAxis;
-//     cosM = cos(omega*simulationTime);
-//
-//     // Get pointer to arrays
-//     latGrad = DUlat->solution;
-//     lonGrad = DUlon->solution;
-//
-//     // Set variables for dUlat solution
-//     latLen = DUlat->fieldLatLen;
-//     lonLen = DUlat->fieldLonLen;
-//
-//     cosLat = DUlat->cosLat;
-//     sinSqLat = DUlat->sinSqLat;
-//     cosSqLon = DUlat->cosSqLon;
-//
-//     // Solve for dUdlat
-//     for (i=0; i<latLen; i++) {
-//         for (j=0; j<lonLen; j++) {
-//             latGrad[i][j] = factor*1.5*cosLat[i]*(5.*cosSqLon[j] * (1.0 - 3.*sinSqLat[i]) - 1.0) * cosM;
-//         }
-//     }
-//
-//     // Set variable for dUlon solution
-//     latLen = DUlon->fieldLatLen;
-//     lonLen = DUlon->fieldLonLen;
-//
-//     cosSqLat = DUlon->cosSqLat;
-//     sinLat = DUlon->sinLat;
-//     sin2Lon = DUlon->sin2Lon;
-//
-//     // Solve for dUdlon
-//     for (i=0; i<latLen; i++) {
-//         for (j=0; j<lonLen; j++) {
-//             lonGrad[i][j] = -factor* 7.5*cosSqLat[i] * sinLat[i] * sin2Lon[j] * cosM;
-//         }
-//     }
-// };
-//
-// /* Degree 3 component of the eccentricity tide (see docs)
-//  *
-//  * Finds the gradient components of the degree-3 eccentricity tide for Fields
-//  * DUlat and DUlon. Only current simulation required is absolute simulation
-//  * time. The function directly writes the solution arrays in both Field
-//  * classes.
-//  *
-//  *      inputs: Field * DUlat           Field for latitude gradient of potential
-//  *              Field * DUlon           Field for longitude gradient of potential
-//  *              double simulationTime   Current time in the simulation
-//  *              double radius           Satellite radius
-//  *              double smAxis           Satellite semimajor axis
-//  *              double omega            Satellite rotational angular speed
-//  *              double ecc              Satellite eccentricity
-//  *
-// */
-// void deg3Ecc(Field * DUlat, Field * DUlon, double simulationTime, double radius, double smAxis, double omega, double ecc) {
-//     double cosM, sinM, factor;                          // cos(Mean anomaly), sin(Mean anomaly)
-//     double * sinLat, * cosLat, * cosSqLat, * sinSqLat;  // Pointer to 1D arrays for trig functions of latitude
-//     double * sinLon, * cosLon, * cosSqLon, * cosCubLon; // Pointer to 1D arrays for trig functions of longitude
-//     double ** latGrad, ** lonGrad;                      // Pointer to 2D array solutions
-//     int i,j;
-//     int latLen, lonLen;
-//
-//     factor = pow(omega,2.0)*pow(radius,3.0)*ecc/smAxis;
-//     cosM = cos(omega*simulationTime);
-//     sinM = sin(omega*simulationTime);
-//
-//     // Get pointer to arrays
-//     latGrad = DUlat->solution;
-//     lonGrad = DUlon->solution;
-//
-//     // Set variables for dUlat solution
-//     latLen = DUlat->fieldLatLen;
-//     lonLen = DUlat->fieldLonLen;
-//
-//     sinLat = DUlat->sinLat;
-//     sinLon = DUlat->sinLon;
-//     cosLon = DUlat->cosLon;
-//     cosSqLat = DUlat->cosSqLat;
-//     cosSqLon = DUlat->cosSqLon;
-//     cosCubLon = DUlat->cosCubLon;
-//
-//     // Solve for dUdlat
-//     for (i=0; i<latLen; i++) {
-//         for (j=0; j<lonLen; j++) {
-//             latGrad[i][j] = -factor * sinLat[i]
-//                             * (3.*sinM*sinLon[j]
-//                             * (15.*cosSqLat[i]*cosSqLon[j] - 1.0)
-//                             + 6. * cosM
-//                             * (5.*cosSqLat[i]*cosCubLon[j] - cosLon[j]));
-//         }
-//     }
-//
-//     // Set variable for dUlon solution
-//     latLen = DUlon->fieldLatLen;
-//     lonLen = DUlon->fieldLonLen;
-//
-//     cosLat = DUlon->cosLat;
-//     cosLon = DUlon->cosLon;
-//     cosSqLat = DUlon->cosSqLat;
-//     sinSqLat = DUlon->sinSqLat;
-//     sinLon = DUlon->sinLon;
-//     cosSqLon = DUlon->cosSqLon;
-//
-//     // Solve for dUdlon
-//     for (i=0; i<latLen; i++) {
-//         for (j=0; j<lonLen; j++) {
-//             lonGrad[i][j] = -factor * cosLat[i]
-//                             * (3.*sinM*cosLon[j]
-//                             * (5.*cosSqLat[i]*(1. - 3.*sinSqLat[i]) - 1.0)
-//                             + 6. * cosM * sinLon[j]
-//                             * (1. - 5. * cosSqLat[i] * cosSqLon[j]));
-//         }
-//     }
-// };
