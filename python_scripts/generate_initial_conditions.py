e6b08844b09cb90f7978334875dd39b8c518ca7f
Hamish Hay <hamishhay.lpl@gmail.com>
2022-11-10 11:29:21 +0000

    Modified

--- python_scripts/generate_initial_conditions.py
+++ python_scripts/generate_initial_conditions.py
@@ -7,10 +7,10 @@ Omega = 2.05e-5
 w = -Omega 
 g = 1.3
 H = 10e3
-obliq = np.deg2rad(-1.0)
-alpha = 1e-8
+obliq = np.deg2rad(-0.01)
+alpha = 1e-7
 m=1
-t = 0.01 * 2*np.pi/w
+t = 0.00 * 2*np.pi/w
 
 lam = w/(2*Omega)
 lambs = 4*Omega**2*R**2 / (g*H)
@@ -32,29 +32,29 @@ def Ln(n):
 Psi11 = U21/(2*Omega) / (qn(1) - Kn(2)*Ln(1)/pn(2))
 Phi21 = -1j*Ln(1)/pn(2) * Psi11
 
-lats = np.arange(0,180.1,0.1)*np.pi/180.0 
-lons = np.arange(0, 360, 0.1)*np.pi/180.0
+lats1d = np.arange(-90,90.1,0.1)*np.pi/180.0 
+lons1d = np.arange(0, 360, 0.1)*np.pi/180.0
 
-lons,lats = np.meshgrid(lons,lats)
+lons,lats = np.meshgrid(lons1d,lats1d)
 
-Y31 = -1.5 * (5*np.cos(lats)**2 - 1)*np.sin(lats)*np.exp(1j * 1 * lons)
+Y31 = 1.5 * (5*np.cos(0.5*np.pi - lats)**2 - 1)*np.sin(0.5*np.pi - lats)*np.exp(1j * 1 * lons)
 
-Y21 = -3 * np.cos(lats)*np.sin(lats) * np.exp(1j * 1 * lons)
+Y21 = 3 * np.cos(0.5*np.pi - lats)*np.sin(0.5*np.pi - lats) * np.exp(1j * 1 * lons)
 
-Y11 = -np.sin(lats)*np.exp(1j*1*lons)
+Y11 = np.sin(0.5*np.pi - lats)*np.exp(1j*1*lons)
 
 U = (-1/3. * (Psi11 * Y21) + 1j*Phi21*Y21)*np.exp(-1j*w*t)
 dUdt = -1j*w*U
 
 U += U.real - 1j*U.imag         # Add complex conjugate
-U /= 2*R*np.sin(lats)
+U /= 2*R*np.sin(0.5*np.pi - lats)
 
 
 V = (1j*Psi11 *Y11 + Phi21*(4*Y31 - 9*Y11)/5.)*np.exp(-1j*w*t)
 V += V.real - 1j*V.imag         # Add complex conjugate 
-V /= 2*R*np.sin(lats)
+V /= 2*R*np.sin(0.5*np.pi - lats)
 
-eta = -1j*6/R**2 * H/w * Phi21 * Y21  * np.exp(-1j*w*t)
+eta = 1j*6/R**2 * H/w * Phi21 * Y21  * np.exp(-1j*w*t)
 eta += eta.real - 1j*eta.imag
 eta *= 0.5
 
@@ -63,9 +63,12 @@ eta *= 0.5
 
 skip = 100
 
-plt.contourf(lons,np.pi*0.5 - lats,eta)
+lons1d = np.rad2deg(lons1d)
+lats1d = np.rad2deg(lats1d)
+
+plt.contourf(lons1d,lats1d,V)
 plt.colorbar()
-plt.quiver(lons[::skip,::skip],np.pi*0.5 -lats[::skip,::skip],U[::skip,::skip],-V[::skip,::skip])
+# plt.quiver(lons1d[::skip],lats1d[::skip],U[::skip,::skip],V[::skip,::skip])
 plt.savefig("init.test.png")
 
 
