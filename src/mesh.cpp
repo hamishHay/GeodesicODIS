a959ed9a548589506f0f71e801af924e1625e45c
hamishHay <hhay@lpl.arizona.edu>
2023-03-31 10:00:15 +0100

    Now reads grid file as defined in gridConstants.h

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -1653,7 +1653,9 @@ int Mesh::ReadGridFile()
 
     int count;
 
-    hid_t file_id = H5Fopen("input_files/test.h5", H5F_ACC_RDONLY, H5P_DEFAULT);
+    std::string file_str = globals->path + SEP + "input_files" + SEP + "grid_l" + std::to_string(|GRID_LVL) + ".h5";
+
+    hid_t file_id = H5Fopen(file_str.c_str(), H5F_ACC_RDONLY, H5P_DEFAULT);
 
     //-----------------------------------------------------------------------------------------
     //------------------------------ LOAD NODE INFO -------------------------------------------
